dea28897fdd4d88b9b074b2c727ba665f2100771
poseidonwood <santosofebrikukuh@gmail.com>
2021-10-22 22:24:02 +0700

    running by bash

--- partial_/footer.php
+++ partial_/footer.php
@@ -25,6 +25,8 @@
             <li><i class="fa fa-angle-right"></i><a href="http://maritimjatim.info/" target="_blank">Stasiun Meteorologi Maritim Perak Surabaya</a></li>
             <li><i class="fa fa-angle-right"></i><a href="http://meteobanyuwangi.info/" target="_blank">Stasiun Meteorologi Banyuwangi</a></li>
             <li><i class="fa fa-angle-right"></i><a href="http://stametbawean.com/" target="_blank">Stasiun Meteorologi Sangkapura Bawean</a></li>
+            <li><i class="fa fa-angle-right"></i><a href="http://stametbawean.com/" target="_blank">Dashboard</a></li>
+
           </div>
 
           <div class="clearfix visible-sm"></div>
