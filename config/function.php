fc42cd39eba0f150b02ff5470eced0e5a2e345fd
poseidonwood <santosofebrikukuh@gmail.com>
2021-10-22 21:17:17 +0700

    adding change

--- config/function.php
+++ config/function.php
@@ -1,5 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Jakarta");
+$filename = './asset/logs/counter.txt';	//mendefinisikan nama file untuk menyimpan counter
 
 function tanggal_indo($tanggal, $cetak_hari = false)
 {
@@ -36,5 +37,18 @@ function tanggal_indo($tanggal, $cetak_hari = false)
   }
   return $tgl_indo;
 }
+
+function counter(){		//function counter
+  global $filename;	//set global variabel $filename
+
+  if(file_exists($filename)){		//jika file counter.txt ada
+    $value = file_get_contents($filename);	//set value = nilai di notepad
+  }else{		//jika file counter.txt tidak ada maka akan membuat file counter.txt
+    $value = 0;		//kemudian set value = 0
+  }
+
+  file_put_contents($filename, ++$value);		//menuliskan kedalam file counter.txt value+1
+}
+
 // echo tanggal_indo('2016-03-20'); // Hasil: 20 Maret 2016;
 // echo tanggal_indo('2016-03-20', true); // Hasil: Minggu, 20 Maret 2016
