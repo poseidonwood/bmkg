8d131c37e2daa45c70b158f05c234d8793b1b07c
poseidonwood <santosofebrikukuh@gmail.com>
2021-10-25 10:09:34 +0700

    running by bash

---
+++ dashboard/helper/koneksi.php
@@ -0,0 +1,28 @@
+<?php
+// source code by ilman sunanuddin
+// documentasi https:/m-pedia.id
+$host = "localhost";
+$username = "root";
+$password = "";
+$db = "bmkg_tuban";
+
+$koneksi = mysqli_connect($host, $username, $password, $db) or die("GAGAL");
+date_default_timezone_set('Asia/Jakarta');
+$isSSL = function () {
+  if (!empty($_SERVER['https']))
+    return true;
+
+  if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+    return true;
+
+  if (!isset($_SERVER['SERVER_PORT']))
+    return false;
+
+  if ($_SERVER['SERVER_PORT'] == 443)
+    return true;
+
+  return false;
+};
+
+$prot = $isSSL() ? 'https://' : 'http://';
+$base_url  = $prot ."localhost/bmkg/dashboard/";
