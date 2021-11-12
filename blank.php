e7340258071c87e4f2acc1260266ae95c51f8df5
poseidonwood <santosofebrikukuh@gmail.com>
2021-11-12 21:04:00 +0700

    running by bash

---
+++ blank.php
@@ -0,0 +1,11 @@
+<?php
+include "./partial_/header.php";
+include "./partial_/navbar.php";
+include "./partial_/breadcrumb.php";
+?>
+<!-- Content disini -->
+<!-- End Content -->
+<?php
+include "./partial_/sidebarpost.php";
+include "./partial_/footer.php";
+?>
