6ec705c488447bed4cbdab8c9a13aed1baa527b9
poseidonwood <santosofebrikukuh@gmail.com>
2021-12-26 07:46:11 +0700

    adding blank.php

--- blank.php
+++ blank.php
@@ -5,24 +5,17 @@ include "./partial_/breadcrumb.php";
 ?>
 <!-- Content disini -->
 <div class="entry-content">
-		<div id="pl-gb3735-618e870f94645"  class="panel-layout" ><div id="pg-gb3735-618e870f94645-0"  class="panel-grid panel-no-style" ><div id="pgc-gb3735-618e870f94645-0-0"  class="panel-grid-cell" ><div id="panel-gb3735-618e870f94645-0-0-0" class="so-panel widget widget_text panel-first-child" data-index="0" >			<div class="textwidget"><p>Sebelum mengisi data, silahkan cek kelengkapan persyaratan Permohonan Data sebagai berikut:</p>
+		<div id="pl-3738"  class="panel-layout" ><div id="pg-3738-0"  class="panel-grid panel-no-style" ><div id="pgc-3738-0-0"  class="panel-grid-cell" ><div id="panel-3738-0-0-0" class="so-panel widget widget_text panel-first-child" data-index="0" >			<div class="textwidget"><p>Sebelum mengisi data, silahkan cek kelengkapan persyaratan Permohonan Data Nol Rupiah sebagai berikut:</p>
 <ol>
 <li>Foto KTP (Kartu Tanda Penduduk) dalam format .jpg</li>
-<li>Surat Permohonan Data atau Surat Pengantar dari Perusahaan/Instansi dalam bentuk .pdf</li>
+<li>Foto KTM (Kartu Tanda Mahasiswa) dalam format .jpg untuk mahasiswa</li>
+<li>Surat Permohonan Data Nol Rupiah atau Surat Pengantar dari Rektor/Dekan/Ketua Jurusan dalam bentuk .pdf. <a href="https://drive.google.com/file/d/1zBA-9tKB2zaNlSaiGHNQShQfc3tTkbsS/view?usp=sharing">[Download Format]</a></li>
+<li>Surat Pernyataan Tarif Nol Rupiah dalam bentuk .pdf. <a href="https://drive.google.com/file/d/1OjaBZjcXf7QTYAsgAzkVEeucdsEPcjhG/view?usp=sharing">[Download Format]</a></li>
+<li>Proposal Penelitian dalam bentuk .pdf</li>
 </ol>
 </div>
-		</div>
-    <div id="panel-gb3735-618e870f94645-0-0-1" class="widget_text so-panel widget widget_custom_html panel-last-child" data-index="1" >
-      <h3 class="widget-title">FORM PERMOHONAN DATA PNBP</h3>
-      <div class="textwidget custom-html-widget">
-        <iframe src="https://www.emailmeform.com/builder/form/x1M1GyJ8L4av5K42r3R4X4u" width="100%" ></iframe>
-        <!-- <script src="https://www.emailmeform.com/builder/forms/jsform/x1M1GyJ8L4av5K42r3R4X4u" type="text/javascript"></script> -->
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-</div><!-- .entry-content -->
+		</div><div id="panel-3738-0-0-1" class="widget_text so-panel widget widget_custom_html panel-last-child" data-index="1" ><h3 class="widget-title">FORM PERMOHONAN DATA NOL RUPIAH</h3><div class="textwidget custom-html-widget"><script src="https://www.emailmeform.com/builder/forms/jsform/Wea9z75810m3gb1" type="text/javascript"></script></div></div></div></div></div>	</div><!-- .entry-content -->
+
 
 <!-- End Content -->
 <?php
