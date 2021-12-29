6fd4f6fc78dbeb7285a977e6e1c15beae108a299
poseidonwood <santosofebrikukuh@gmail.com>
2021-12-29 21:45:54 +0700

    adding config.php

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php 
+$mode = "production";
+
+?>
--- dashboard/helper/koneksi.php
+++ dashboard/helper/koneksi.php
@@ -1,10 +1,18 @@
 <?php
-// source code by ilman sunanuddin
-// documentasi https:/m-pedia.id
-$host = "localhost";
-$username = "root";
-$password = "";
-$db = "bmkg_tuban";
+
+include("../../config.php");
+if($mode == "production"){
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$db = "bmkg_tuban";
+}else{
+	$host = "localhost";
+	$username = "cuacatu1_wp670";
+	$password = "Met96939";
+	$db = "cuacatu1_wp670";
+
+}
 
 $koneksi = mysqli_connect($host, $username, $password, $db) or die("GAGAL");
 date_default_timezone_set('Asia/Jakarta');
